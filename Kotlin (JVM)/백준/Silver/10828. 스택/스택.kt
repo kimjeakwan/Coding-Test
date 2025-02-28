fe0b74c8d1e60d174fa6884e897afe08ceb7e082
kimjeakwan <165874745+kimjeakwan@users.noreply.github.com>
2025-03-01 00:00:14 +0900

    [Silver IV] Title: 스택, Time: 132 ms, Memory: 17524 KB -BaekjoonHub

--- Kotlin (JVM)/백준/Silver/10828. 스택/스택.kt
+++ Kotlin (JVM)/백준/Silver/10828. 스택/스택.kt
@@ -10,10 +10,10 @@ fun main() {
     var ptr = -1
  
     fun push(n: Int) {
-      stack[++ptr] = n
+      stack[++ptr] = n //정수 x 를  순서대로 스택에 넣는다. 0번쨰부터
     }
     fun pop(): Int {
-        return if(ptr==-1) -1
+        return if(ptr==-1) -1 // ptr 이 비어있으면 -1 이기떄문에 -1 를 봔한하고 가장위에있는 정수 인 stack[ptr]을 빼고 후위연사자 이기때문에 결과적으로 stack[--ptr] 가 남는다 
         else stack[ptr--]
     }
     fun size(): Int = ptr + 1
